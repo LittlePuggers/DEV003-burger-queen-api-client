14d1d6a1cddf2d5b923f54f6ce2bd87d20366ff1
LittlePuggers <anaim.ecologia@gmail.com>
2023-04-24 07:40:19 -0700

    trying tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthService } from 'src/services/auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +12,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [HttpClientTestingModule, HttpClient, HttpClientModule],
+      providers: [AuthService]
     })
     .compileComponents();
 
