8ecad36deb8f3b6372c596d04faf172d2515137b
LittlePuggers <anaim.ecologia@gmail.com>
2023-04-26 00:20:18 -0700

    Updates

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DEV003-burger-queen-api-client';
+}
