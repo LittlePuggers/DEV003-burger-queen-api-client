ea1e65a0c8fb74ea8964887d5c5eeedf70cc12f2
LittlePuggers <anaim.ecologia@gmail.com>
2023-05-02 12:39:36 -0700

    menu view ready

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,28 +1,59 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+interface Product {
+  id: number,
+  dateEntry: string,
+  image: string,
+  name: string,
+  price: number,
+  type: string
+}
+
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-
   styleUrls: ['./product-card.component.css']
 })
+
 export class ProductCardComponent {
-  products = [{
+  products: Array<Product> = [{
+    id: 0,
+    dateEntry: '',
     image: '',
     name: '',
     price: 0,
+    type: ''
   }]
-  constructor(private http: HttpClient) { this.showProducts() } // Muestra los productos en la consola
+
+  breakfastItems: Product[] = []
+  lunchItems: Product[] = []
+
+  constructor(private http: HttpClient) { 
+    // this.showBreakfast()
+    this.showProducts() 
+  } // Muestra los productos en la consola
+  
   getProducts(): Observable<any> {
-    console.log(this.getProducts)
+    // console.log(this.getProducts)
     return this.http.get('http://localhost:3000/products')
   }
+  
   showProducts(): void {
     this.getProducts().subscribe((products) => {
       this.products = products;
-      console.log(products);
+      this.breakfastItems = this.products.filter((product)=> 
+        product.type === 'Desayuno'
+      )
+      this.lunchItems = this.products.filter((product)=>{
+        return product.type === 'Resto del día'
+      })
+      console.log(this.products);
+      console.log(this.breakfastItems);
+      console.log(this.lunchItems)
     });
   }
+  
 }
 
