39289bc57e38dbdd6688940faf2baa49c2e4aecb
LittlePuggers <anaim.ecologia@gmail.com>
2023-05-04 15:04:25 -0700

    order component init

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NgModel } from '@angular/forms';
+
+interface Order {
+  id: number,
+  userId: string,
+  client: string,
+  products: [
+    {
+      qty: number,
+      product: {
+        id: number,
+        name: string,
+        price: number,
+        image: string,
+        type: string,
+        dateEntry: string,
+      }
+    }
+  ],  
+}
 
 @Component({
   selector: 'app-order',
@@ -6,5 +28,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent {
+  orders: Array<Order> = [{
+    id: 0,
+    userId: '',
+    client: '',
+    products: [
+      {
+        qty: 0,
+        product: {
+          id: 0,
+          name: '',
+          price: 0,
+          image: '',
+          type: '',
+          dateEntry: '',
+        }
+      }
+    ],  
+  }]
 
+  newOrder = {
+    id: 0,
+    userId: '',
+    client: '',
+    products: [
+      {
+        qty: 0,
+        product: {
+          id: 0,
+          name: '',
+          price: 0,
+          image: '',
+          type: '',
+          dateEntry: '',
+        }
+      }
+    ],  
+  };
+
+  constructor(private http: HttpClient) {
+    this.showOrders() // Muestra los productos en la consola
+  } 
+  
+  addProduct(product:any, qty:number) {
+    this.newOrder.products.push({ product, qty });
+  }
+  
+  // Otros métodos para agregar detalles a la nueva orden aquí
+
+  getOrders(): Observable<any> {
+    return this.http.get('http://localhost:3000/orders')
+  }
+  
+  showOrders(): void {
+    this.getOrders().subscribe((orders) => {
+      this.orders = orders;
+      console.log(orders)
+    });
+  }
 }
